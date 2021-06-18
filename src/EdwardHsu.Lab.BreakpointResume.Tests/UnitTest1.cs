086df569bc4f94ae3eb14f94a0e6847c260885bc
Edward Hsu <st89382000@gmail.com>
2021-06-18 23:37:24 +0800

    init

---
+++ src/EdwardHsu.Lab.BreakpointResume.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+
+using Xunit;
+
+namespace EdwardHsu.Lab.BreakpointResume.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
